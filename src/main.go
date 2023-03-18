607ea101a0e4344fbe38b245d024e0e4571fa12b
appithe <appithe@github.com>
2023-03-17 21:26:39 -0600

    Operadores aritmeticos

--- src/main.go
+++ src/main.go
@@ -3,35 +3,42 @@ package main
 import "fmt" // paquete para imprimir en consola
 
 func main() {
-	// $go build #para compilar el código y generar el archivo build
-	// $go run #para ejecutar el código sin compilar
-
-	//Declaración de constantes
-	const pi float64 = 3.14
-	const pi2 = 3.1415
-
-	fmt.Println("pi: ", pi)
-	fmt.Println("pi2: ", pi2)
-
-	// Declaración de variables enteras
-	base := 12          // variable no declarada anteriormente
-	var altura int = 14 // declaramos con tipo de dato y asignamos un valor
-	var area int
-
-	fmt.Println(base, altura, area)
-
-	// Zero values
-	var a int     // 0
-	var b float64 // 0
-	var c string  // " "
-	var d bool    // false
-
-	fmt.Println(a, b, c, d)
 
 	// Area cuadrado
 	const baseCuadrado = 10
 	areaCuadrado := baseCuadrado * baseCuadrado
 
 	fmt.Println("Area cuadrado: ", areaCuadrado)
+
+	x := 10
+	y := 50
+
+	// Suma
+	result := x + y
+	fmt.Println("Suma: ", result)
+
+	// Resta
+	result = y - x
+	fmt.Println("Resta: ", result)
+
+	// Multiplicación
+	result = x * y
+	fmt.Println("Multiplicación: ", result)
+
+	// Division
+	result = y / x
+	fmt.Println("Division: ", result)
+
+	// Modulo
+	result = y % x
+	fmt.Println("Modulo: ", result)
+
+	// Incrementar
+	x++
+	fmt.Println("Incrementar: ", x)
+
+	// Decrementar
+	x--
+	fmt.Println("Decrementar: ", x)
 
 }
