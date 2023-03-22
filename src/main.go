e29a977414c3629b387bc5b18c164207e63fb7af
appithe <appithe@github.com>
2023-03-21 18:55:10 -0600

    Uso de funciones

--- src/main.go
+++ src/main.go
@@ -2,29 +2,36 @@ package main
 
 import "fmt" // paquete para imprimir en consola
 
-func main() {
-	// declaración de variables
-
-	helloMessage := "Hello"
-	worldMessage := "World"
-
-	// Println
-	fmt.Println(helloMessage, worldMessage)
-	fmt.Println(helloMessage, worldMessage)
-
-	// Printf
-	nombre := "Platzi"
-	cursos := 500
-
-	fmt.Printf("%s tiene mas de %d cursos\n", nombre, cursos) // %s para strings y %d para enteros
-	fmt.Printf("%v tiene mas de %v cursos\n", nombre, cursos) // %v si no sabemos que tipo de dato estará ahi
-
-	// Sprintf --> guarda el mensaje en una variable para después imprimirlo
-	message := fmt.Sprintf("%s tiene mas de %d cursos", nombre, cursos)
+// Se usa camelCase
+func normalFunction(message string) {
 	fmt.Println(message)
+}
 
-	// Tipo de dato --> typeof en JS
-	fmt.Printf("helloMessage: %T\n", helloMessage)
-	fmt.Printf("cursos: %T", cursos)
+func tripleArgument(a, b int, c string) {
+	fmt.Println(a, b, c)
+}
 
+func returnValue(a int) int {
+	return a * 2
+}
+
+func doubleReturn(a int) (c, d int) {
+	return a, a * 2
+}
+
+func main() {
+	normalFunction("Hola mundo")
+
+	tripleArgument(1, 2, "hola")
+
+	value := returnValue(2)
+	fmt.Println("value: ", value)
+
+	/* value1, value2 := doubleReturn(2)
+	fmt.Println("value1 y value2: ", value1, value2) */
+
+	value1, _ := doubleReturn(2)
+	fmt.Println("value1: ", value1)
+
+	// Reto pasar a funciones el reto de calcular areas
 }
