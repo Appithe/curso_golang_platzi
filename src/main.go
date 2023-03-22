32df847601226ab3d5a6103210232da1f2318c66
appithe <appithe@github.com>
2023-03-21 20:22:49 -0600

    reto solucionado

--- src/main.go
+++ src/main.go
@@ -18,13 +18,20 @@ func main() {
 		counter++
 	}
 
-	fmt.Print("\n")
+	// fmt.Print("\n")
 
 	// For forever
-	counterForever := 0
+	/*counterForever := 0
 	for {
 		fmt.Println(counterForever)
 		counterForever++
+	} */
+
+	fmt.Print("\n")
+
+	// Reto, contador decrementador
+	for i := 10; i > 0; i-- {
+		fmt.Print(i)
 	}
 
 }
